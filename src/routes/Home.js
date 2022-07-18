bda060839da7f34290d89aeadd331ae4af645469
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-18 13:28:52 +0200

    removing movies list form home(it's managed now by state at App comp

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,55 +2,22 @@ import React, {Component} from "react";
 import { HeaderImg, LoadButton, Searchbar } from "../components";
 import { PosterList } from "../components/PosterList";
 
-const movies = [
-    {
-      backdrop_path: './images/Fast_large.jpg',
-      id: 475557,
-      overview:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
-      poster_path: './images/Fast_small.jpg',
-      title: "Fast and Furious"
-    },
-    {
-        backdrop_path: './images/Fast_large.jpg',
-        id: 475558,
-        overview:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
-        poster_path: './images/Fast_small.jpg',
-        title: "Fast and Furious"
-      },
-      {
-        backdrop_path: './images/Fast_large.jpg',
-        id: 475559,
-        overview:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
-        poster_path: './images/Fast_small.jpg',
-        title: "Fast and Furious"
-      },
-      {
-        backdrop_path: './images/Fast_large.jpg',
-        id: 475554,
-        overview:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
-        poster_path: './images/Fast_small.jpg',
-        title: "Fast and Furious"
-      },
-  ];
   
 
 class Home extends Component {
     render() {
+        const {mTitle, mDesc, image, movies, loading } = this.props;
         return (
           <div>
                 <HeaderImg 
-                    title="Fast and Furious"
-                    overview="Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
-                    imgSrc={'./images/Fast_large.jpg'}
+                    title={mTitle}
+                    overview={mDesc}
+                    imgSrc={image}
 
                 />
                 <Searchbar />
                 <PosterList movies={movies}/>
-                <LoadButton loading={false}/>
+                <LoadButton loading={loading}/>
            </div>
         )
     }
