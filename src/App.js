40dbd669ab808a8e93c49548399d5ddb666d8968
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-18 13:28:14 +0200

    defining state at App comp

--- src/App.js
+++ src/App.js
@@ -4,12 +4,59 @@ import './App.css';
 import { Home } from "./routes/Home";
 
 class App extends Component {
+
+  state = {
+    loading:false,
+    movies:[
+      {
+        backdrop_path: './images/Fast_large.jpg',
+        id: 475557,
+        overview:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
+        poster_path: './images/Fast_small.jpg',
+        title: "Fast and Furious"
+      },
+      {
+          backdrop_path: './images/Fast_large.jpg',
+          id: 475558,
+          overview:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
+          poster_path: './images/Fast_small.jpg',
+          title: "Fast and Furious"
+        },
+        {
+          backdrop_path: './images/Fast_large.jpg',
+          id: 475559,
+          overview:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
+          poster_path: './images/Fast_small.jpg',
+          title: "Fast and Furious"
+        },
+        {
+          backdrop_path: './images/Fast_large.jpg',
+          id: 475554,
+          overview:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis in quos impedit? Iure officia fugit nostrum blanditiis! Blanditiis voluptatem consectetur, eos fugiat explicabo maiores, optio perferendis rem quas perspiciatis quos?",
+          poster_path: './images/Fast_small.jpg',
+          title: "Fast and Furious"
+        },
+    ],
+    badge:0,
+    image:'./images/Fast_large.jpg',
+    mTitle:'Fast and Furious',
+    mDesc: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+    activePage:0,
+    totalPages:0,
+    searchText:""
+  }
   
   render() {
     return (
       <div className="App">
-         <Header badge={18} />
-         <Home />
+         <Header badge={this.state.badge} />
+         <Home 
+           {...this.state}
+          />
       </div>
     );
   }
