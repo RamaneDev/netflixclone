5181552d34d07891d354cfb626e03cdbccc08788
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-18 13:25:09 +0200

    defining state for searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,14 +3,25 @@ import FontAwesome from "react-fontawesome";
 import '../css/SearchBar.css';
 
 class Searchbar extends Component {
+
+    state = {
+        value:""
+    }
+    handleChange = e => {
+        this.setState({
+            value: e.target.value
+        })
+    }
     render() {
         return (
             <div className="searchBar--container">
                 <div className="searchBar">
                   <input
                      className="searchBar--input"
+                     value={this.state.value}
                      type="text"
-                     placeholder="Rechercher un film" />
+                     placeholder="Rechercher un film"
+                     onChange={this.handleChange} />
                 </div>
                 <div className="searchBar--submit">
                     <FontAwesome name="search" />
