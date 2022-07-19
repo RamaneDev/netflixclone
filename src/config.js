17ebf6127f2f02a0fa99f53581d358a54095c431
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-19 13:51:46 +0200

    setting up the configuration for calling themoviedb

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://api.themoviedb.org/3';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+
+const API_KEY = '93fda456a5674d02ea11f76db94eea95';
+
+const BACKDROP_SIZE = 'w1280';
+
+const POSTER_SIZE = 'w500';
+
+export {
+    API_URL,
+    IMAGE_BASE_URL,
+    API_KEY,
+    BACKDROP_SIZE,
+    POSTER_SIZE
+}
