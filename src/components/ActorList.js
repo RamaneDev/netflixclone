26112372767eed77ca64859fa0fe044cd52a4252
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-20 14:45:39 +0200

    implementing renderActor for ActorList comp

--- src/components/ActorList.js
+++ src/components/ActorList.js
@@ -3,7 +3,16 @@ import React from "react";
 const ActorList = (props) => {
 
     const renderActor = () => {
-
+         return props.actors.map((actor, i) => {
+             const imgSrc = './images/Fast_small.jpg';
+             return (
+                 <Actor
+                    key={i}
+                    imgSrc={imgSrc}
+                    name={actor.name}
+                 />
+             )
+         })
     }
     return (
         <div className="actorList">
