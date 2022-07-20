18a7894e083be74fdd06d60d9f8260a5fe4e67b6
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-20 15:28:38 +0200

    finalize details comp

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,10 +1,44 @@
 import React, {Component} from "react";
+import { act } from "react-dom/test-utils";
+import { ActorList, HeaderDetails, Spinner } from "../components";
+
+const actors = [
+    {
+        name:"mesri batman"
+    },
+    {
+        name:"omar khatab "
+    },
+    {
+        name:"bruce lai"
+    },
+    {
+        name:"kody cactus"
+    }
+]
 
 class Details extends Component {
     render() {
         return (
-            <div>
-                je suis le compoant Details
+            <div className="app">
+                {this.props.loading?
+                (
+                    <Spinner />
+                ): (
+                    <>
+                    <HeaderDetails 
+                      mTitle={"Batman"}
+                      mDesc={"voici la description du film batman"}
+                      imgSrc={'./images/Fast_large.jpg'}
+                      runtime={'2h30'}
+                      revenue={'$11548'}
+                      status={'Released'}
+                      vote={''}
+                    />
+                    
+                    <ActorList actors={actors} />
+                    </>
+                )}
             </div>
         )
     }
