929c3f9405c37c875f42676c688bf806314a711c
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-07-24 12:26:52 +0200

    Call themoviedb api from Details & set it's State

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,35 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { ActorList, HeaderDetails, Spinner } from "../components";
+import { API_KEY, API_URL } from "../config";
 
 
 
-function Details() {
-    const[state, setState] = useState({
-        loading:false,
-        actors:[
-            {
-                name:"mesri batman"
-            },
-            {
-                name:"omar khatab "
-            },
-            {
-                name:"bruce lai"
-            },
-            {
-                name:"kody cactus"
-            }
-        ],
-        mTitle: 'Batman',
-        mDesc:'Voici la description du film batman',
-        imgSrc:'./images/Fast_large.jpg',
-        revenue:'$12545',
-        runtime:'2h30',
-        status:'Released',
+function Details(props) {
+    
+    const[State, SetState] = useState({
+        loading:true,
+        actors:[],
+        mTitle: '',
+        mDesc:'',
+        imgSrc:'',
+        revenue:'',
+        runtime:'',
+        status:'',
         vote:''
     });
     
-    const {loading, mTitle, mDesc, actors, imgSrc, revenue, runtime, status, vote } = state;
+    const {loading, mTitle, mDesc, actors, imgSrc, revenue, runtime, status, vote } = State;
+
+    const id = useParams().id;
+
+
+    useEffect(() => {     
+      
+        
+
+        const loadInfo = async () => {
+                
+            const url =`${API_URL}/movie/${id}?api_key=${API_KEY}&language=fr`;  
+            const urlCredis =`${API_URL}/movie/${id}/credits?api_key=${API_KEY}&language=fr`;
+            
+            const res = await fetch(url);
+            const credis_res = await fetch(urlCredis);
+
+            return [await res.json(), await credis_res.json()];
+
+            
+        }       
+          
+        loadInfo().then(
+            (res) => {              
+                const {                      
+                       revenue,
+                       runtime, 
+                       title, 
+                       overview, 
+                       status, 
+                       vote_average, 
+                       poster_path
+                    
+                } = res[0];
+
+                const {
+               
+                        cast
+                 
+                } = res[1];
+
+
+                const newState = {
+                    loading:false,
+                    actors: [...cast],
+                    mTitle: title,
+                    mDesc: overview,
+                    imgSrc:poster_path,
+                    revenue:revenue,
+                    runtime:runtime,
+                    status,
+                    vote:vote_average
+                };
+
+              SetState(newState);               
+            }
+        );
+    }, [id]);
+
+  
+
+ 
 
     return (
         <div className="app">
