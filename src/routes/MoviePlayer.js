e9293ed2c7520c3720b6bdf4e29948df30fe3ef9
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-10 01:55:10 +0200

    getting list of favorit movies from store

--- src/routes/MoviePlayer.js
+++ src/routes/MoviePlayer.js
@@ -1,69 +1,28 @@
 import React from "react"
 import { MvPlayerList, VideoPlayer } from "../components"
 import '../css/MoviePlayer.css'
+import store from '../store';
 
 
 
 const MoviePlayer = (props) => {
     
-    
-    const selectedMovie = {
-        duration : '2h 9m',
-        id: 429617,
-        imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
-        position: 1,
-        title: 'Spider-Man : Far from home',
-        videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
 
-    }
+    const state = store.getState()
+    const movies = Object.values(state.movies.entities)
+    const selectedMovie = movies[0]
     
-    const movies = [
-        {
-            duration : '2h 9m',
-            id: 429617,
-            imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
-            position: 1,
-            title: 'Spider-Man : Far from home',
-            videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    // const selectedMovie = {
+    //     duration : '2h 9m',
+    //     id: 429617,
+    //     imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
+    //     position: 1,
+    //     title: 'Spider-Man : Far from home',
+    //     videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+
+    // }
     
-        },
-        {
-            duration : '2h 9m',
-            id: 429618,
-            imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
-            position: 1,
-            title: 'Spider-Man : Far from home',
-            videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
     
-        },
-        {
-            duration : '2h 9m',
-            id: 429619,
-            imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
-            position: 1,
-            title: 'Spider-Man : Far from home',
-            videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-    
-        },
-        {
-            duration : '2h 9m',
-            id: 429620,
-            imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
-            position: 1,
-            title: 'Spider-Man : Far from home',
-            videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-    
-        },
-        {
-            duration : '2h 9m',
-            id: 429621,
-            imageUrl: 'http://image.tmdb.org/t/p/w1280/5myQbDzw3l8K9yofUXRJ4UTVgam.jpg',
-            position: 1,
-            title: 'Spider-Man : Far from home',
-            videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-    
-        }
-    ]
 
     return(
         <div className="moviePlayer">
