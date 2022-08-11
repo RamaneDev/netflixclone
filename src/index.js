d1a18e3501f836f8e3008a15b2721b2cba6bddc0
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-11 12:54:20 +0200

    from react 18 to react 17 to use firebaseui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 import { getMovies } from './features/movies/moviesSlice';
 import store from './store';
 
 store.dispatch(getMovies())
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-       <App />
-    </Provider>
-  </React.StrictMode>
-);
+ReactDOM.render(<React.StrictMode>
+  <Provider store={store}>
+     <App />
+  </Provider>
+</React.StrictMode>, 
+
+      document.getElementById('root'));
